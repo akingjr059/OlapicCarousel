4238deb91fd37867336c3b9ce46ed1a0f4bfdd25
Anthony King Jr <akingjr059@gmail.com>
2015-10-27 13:30:41 -0400

    Finished!!
    Finally got the response to be read correctly.  If I have some extra
    time I will add some styling to the left and right buttons, background,
    etc.

--- sliderscript.js
+++ sliderscript.js
@@ -27,8 +27,13 @@ var right = function(){
 //XMLHttp Request from the Olapic API
 var getData = function() {
 	
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "http://photorankapi-a.akamaihd.net/customers/215757/media/recent/?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18", false);
+	var xhr = new XMLHttpRequest(),
+		photoListFrag = document.createDocumentFragment(),
+		srcLink,
+		image,
+		listItem;
+	
+	xhr.open("GET", "http://photorankapi-a.akamaihd.net/customers/215757/media/recent/?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18", true);
 	xhr.send();
 	
 	var img = xhr.response.message;
